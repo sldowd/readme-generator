9af41948c315b85e291eac1b9a12d0a290914628
Sarah Dowd <sarahlynnedowd@gmail.com>
2021-01-12 12:56:03 -0500

    update

--- Develop/index.js
+++ Develop/index.js
@@ -1,10 +1,18 @@
 // TODO: Include packages needed for this application
-
+const fs = require('fs');
+const inquirer = require('inquirer');
+const generateReadme = require('./src/readme-template.js')
 // TODO: Create an array of questions for user input
 const questions = [];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile('readme.md', generateReadme(data), err => {
+        if(err) throw err;
+
+        console.log('readme generated');
+    });
+}
 
 // TODO: Create a function to initialize app
 function init() {}
